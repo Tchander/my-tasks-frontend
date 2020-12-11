3462cae6beb72607353f94fba31bb17a1f140ac7
Tchander <evgeniy.tchander@yandex.ru>
2020-12-11 14:33:51 +0300

    master:add to production

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,11 +1,10 @@
 import axios from 'axios'
+import { environment } from '../environments/environment';
 
 let API_URL = 'http://localhost:3000'
-// if (process.env.NODE_ENV === 'production') {
-//   API_URL = 'https://cryptic-stream-50751.herokuapp.com/'
-// } else {
-//   API_URL = 'http://localhost:3000'
-// }
+if (environment.production) {
+  API_URL = 'https://cryptic-stream-50751.herokuapp.com/'
+}
 
 export default axios.create({
   baseURL: API_URL,
